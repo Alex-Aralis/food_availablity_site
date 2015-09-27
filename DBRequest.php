f27b24efc43a795f88fe7abceb49074c2fd08722
Alex Aralis <alex@gmail.com>
2015-09-27 14:10:09 -0700

    Start of the PDO branch.

--- DBRequest.php
+++ DBRequest.php
@@ -6,19 +6,19 @@ try {
     $conn = new PDO("mysql:host=localhost;dbname=food", $_POST['userName'], 
          $_POST['password']);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//  $conn->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);  already the default?
     $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //returns fetches
          //as correct types
 
     if(array_key_exists("query", $_POST) && $_POST['query']!=''){
+        $query = PDO::quote($_POST['query']);
         $stmt = $conn->query(
-             "select latitude,longitude from farmers_markets where {$_POST['query']}");
-        error_log("query: {$_POST['query']}\n", 3, $error_file);
+             "select latitude,longitude from farmers_markets where $query");
+        error_log("query: $query\n", 3, $error_file);
     }else{
         $stmt = $conn->query("select latitude,longitude from farmers_markets");
     }
     
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+    $result = $stmt->fetchAll(PDO::FETCH_NUM); 
 
     echo json_encode($result);
 
